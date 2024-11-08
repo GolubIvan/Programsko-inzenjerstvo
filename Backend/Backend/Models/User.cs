342a2bf5064f883cc79aa3a0dff18effbbc0c5ee
Vedran Vrabec <vv54628@fer.hr>
2024-11-08 12:39:36 +0100

    u klasi User je dodan funkcija changePassword

--- Backend/Backend/Models/User.cs
+++ Backend/Backend/Models/User.cs
@@ -53,5 +53,37 @@ namespace Backend.Models
             }
             return userId;
         }
+
+        public Boolean changePassword(int userId, string oldPassword, string newPassword)
+        {
+            if (checkPassword(userId, oldPassword))
+            {
+                var conn = Database.GetConnection();
+                using (var cmd = new NpgsqlCommand("UPDATE korisnik SET lozinka = @newPassword WHERE userId = @id", conn))
+                {
+                    cmd.Parameters.AddWithValue("newPassword", BCrypt.Net.BCrypt.HashPassword(newPassword));
+                    cmd.Parameters.AddWithValue("id", userId);
+                    cmd.ExecuteNonQuery();
+                }
+                return true;
+            }
+            else
+                return false;
+          
+        }
+
+        public List<Meeting> dohvatiMeetinge(int zgradaId)
+        {
+            var conn = Database.GetConnection();
+            List<Meeting> meetings = new List<Meeting>();
+            var cmd = new NpgsqlCommand("SELECT * FROM meeting WHERE zgradaId = @id", conn);
+            cmd.Parameters.AddWithValue("id", zgradaId);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                meetings.Add(new Meeting(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetDateTime(2)));
+            }
+            return meetings;
+        }
     }
 }
