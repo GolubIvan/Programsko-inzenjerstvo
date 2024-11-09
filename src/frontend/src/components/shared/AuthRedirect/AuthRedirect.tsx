9e0c07ace5c895b0411569a50c2b5fede3b0e17f
slazarusic <sara.lazarusic@fer.unizg.hr>
2024-11-09 23:14:03 +0100

    inicijalni auth redirect

---
+++ src/frontend/src/components/shared/AuthRedirect/AuthRedirect.tsx
@@ -0,0 +1,30 @@
+import { authFetcher } from "@/fetchers/fetcher";
+import { swrKeys } from "@/typings/swrKeys";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import useSWR from "swr";
+
+interface IAuthRedirect {
+  to: string;
+  condition: "isLoggedIn" | "isLoggedOut";
+  role: "admin" | "predstavnik" | "suvlasnik";
+}
+
+export default function AuthRedirect({ to, condition }: IAuthRedirect) {
+  const route = useRouter();
+  const { data, isLoading } = useSWR(swrKeys.me, authFetcher);
+
+  useEffect(() => {
+    if (isLoading) return;
+    console.log(data);
+    console.log(condition);
+    if (!data && condition == "isLoggedOut") {
+      route.push(to);
+    }
+    if (data && condition == "isLoggedIn") {
+      route.push(to);
+    }
+  }, [data, isLoading, to, condition, route]);
+
+  return null;
+}
