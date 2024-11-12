3bed12003ca4f29cddd2a4534d495fdd33e79432
ivan <ig54902>
2024-11-12 19:50:52 +0100

    update Dockerfile, gitignore, povezivanje s bazom

--- Backend/Backend/Models/Database.cs
+++ Backend/Backend/Models/Database.cs
@@ -3,6 +3,14 @@ using Npgsql;
 
 public static class Database
 {
+    
+    //s bazom online
+    //private static string connString = "Host=dpg-csotlkhu0jms738psho0-a.frankfurt-postgres.render.com;Port=5432;Username=dev;Password=uUxLrHBxx9QDyrzExGKshF2KyAUhtzLT;Database=ezgrada_db";
+
+    //ako na dockeru se testira
+    //private static readonly string connString = "Host=host.docker.internal;Username=dev;Password=baze;Database=eZgrada";
+
+    //lokalna baza
     private static readonly string connString = "Host=localhost;Username=postgres;Password=baze;Database=eZgrada";
     private static NpgsqlConnection? connection;
 
