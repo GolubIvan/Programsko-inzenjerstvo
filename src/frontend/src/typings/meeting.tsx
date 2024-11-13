9c871d23478fdfc4bb536ba44ab44bf9c12e6e09
mrossini31 <marin.rossini@fer.unizg.hr>
2024-11-13 15:37:11 +0100

    Preimenovani src i backend

--- src/frontend/src/typings/meeting.tsx
+++
@@ -1,8 +0,0 @@
-export interface IMeetingSummary {
-  title: string;
-  id: string;
-  time: string;
-  place: string;
-  summary: string;
-  status: "obavljen" | "objavljen" | "planiran" | "arhiviran";
-}
