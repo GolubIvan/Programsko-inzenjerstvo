7b96c8dee6383bf1a2d3a9626bea359644812063
ivan <ig54902>
2024-12-29 18:14:39 +0100

    novi link za online bazu

--- Backend/Backend/Models/Database.cs
+++ Backend/Backend/Models/Database.cs
@@ -3,15 +3,15 @@ using Npgsql;
 
 public static class Database
 {
-    
+
     //s bazom online
-    //private static string connString = "Host=dpg-csotlkhu0jms738psho0-a.frankfurt-postgres.render.com;Port=5432;Username=dev;Password=uUxLrHBxx9QDyrzExGKshF2KyAUhtzLT;Database=ezgrada_db";
+    private static string connString = "Host=dpg-ctonallsvqrc73b9u04g-a.frankfurt-postgres.render.com;Port=5432;Username=dev;Password=WK9XC2jNA8bWzJmCfZULPiuLeBZDc0EH;Database=ezgrada_db_2ql1";
 
     //ako na dockeru se testira
     //private static readonly string connString = "Host=host.docker.internal;Username=dev;Password=baze;Database=eZgrada";
 
     //lokalna baza
-    private static readonly string connString = "Host=localhost;Username=postgres;Password=baze;Database=eZgrada";
+    //private static readonly string connString = "Host=localhost;Username=postgres;Password=baze;Database=eZgrada";
     private static NpgsqlConnection? connection;
 
     public static NpgsqlConnection GetConnection()
