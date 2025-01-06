78b42326a404ca45c45f7d38f07019954a34152f
Vedran Vrabec <vv54628@fer.hr>
2025-01-06 18:20:41 +0100

    dodano da sve tocke dnevnog reda koje imaju pravni ucinak moraju biti ili izglasane ili odbijene

--- Backend/Backend/Controllers/MeetingController.cs
+++ Backend/Backend/Controllers/MeetingController.cs
@@ -445,7 +445,7 @@ namespace Backend.Controllers
             }
 
             foreach(var tocka in meeting.tockeDnevnogReda){
-                if(tocka.imaPravniUcinak == true && tocka.stanjeZakljucka != "Izglasan") { return BadRequest(new { error = "Invalid change", message = "Tocke with pravni ucinak have to be Izglasan." }); }
+                if(tocka.imaPravniUcinak == true && (tocka.stanjeZakljucka != "Izglasan" || tocka.stanjeZakljucka != "Odbijen")) { return BadRequest(new { error = "Invalid change", message = "Tocke with pravni ucinak have to be Izglasan or Odbijen." }); }
             }
 
             string email = JWTGenerator.ParseGoogleJwtToken(token);
