1de3345c14e0a1cee3b15e95b1b8b180b97ca5cf
Vedran Vrabec <vv54628@fer.hr>
2025-01-13 15:47:15 +0100

    u apicontrolleru unaprijedio funkcionalnosti; stvorio MeetingRequestAPI

--- izvorniKod/backend/Backend/Controllers/APIController.cs
+++ izvorniKod/backend/Backend/Controllers/APIController.cs
@@ -1,6 +1,8 @@
 using Backend.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Xml;
+using Newtonsoft.Json;
 
 namespace Backend.Controllers
 {
@@ -8,23 +10,40 @@ namespace Backend.Controllers
     public static class APIController : Controller
     {
         [HttpPost("")]
-        public IActionResult Test([FromBody] MeetingRequest meetingRequest)
+        public IActionResult Test([FromBody] MeetingRequestAPI meetingRequest)
         {
-            string apiKey = Request.Headers["apiKey"].ToString() ?? "";
-       
-            if (token == "undefined" || token == "")        //postoji token
-            {
+            string apiKeyGet = Request.Headers["apiKey"].ToString() ?? "";
+            string apiKey = "nas_api_key";
+            if(apiKeyGet == null || apiKeyGet != apiKey)
                 return Unauthorized(new { error = "Invalid API key", message = "The API key is invalid or has expired." });
-            }
 
             if (meetingRequest == null)                     //postoji meeting
             {
                 return BadRequest(new { error = "Invalid data", message = "Meeting data required." });
             }
-            
+            Backend.Models.MeetingRequest meetingRequestDobar = new Backend.Models.MeetingRequest();
+            meetingRequestDobar.Naslov = meetingRequest.Naslov;
+            meetingRequestDobar.Mjesto = meetingRequest.Mjesto;
+            meetingRequestDobar.Opis = meetingRequest.Opis;
+            meetingRequestDobar.Vrijeme = meetingRequest.Vrijeme;
+            meetingRequestDobar.Status = meetingRequest.Status;
+            meetingRequestDobar.ZgradaId = meetingRequest.getZgradaId();
+            meetingRequestDobar.Sazetak = meetingRequest.Sazetak;
+            meetingRequestDobar.TockeDnevnogReda = new List<TockaDnevnogRedaRequest>();
+            foreach (var tocka in meetingRequest.TockeDnevnogReda)
+            {
+                TockaDnevnogRedaRequest tockaDobar = new TockaDnevnogRedaRequest();
+                tockaDobar.ImeTocke = tocka.ImeTocke;
+                tockaDobar.ImaPravniUcinak = tocka.ImaPravniUcinak;
+                tockaDobar.SazetakRasprave = tocka.SazetakRasprave;
+                tockaDobar.StanjeZakljucka = tocka.StanjeZakljucka;
+                tockaDobar.LinkNaDiskusiju = tocka.LinkNaDiskusiju;
+                meetingRequestDobar.TockeDnevnogReda.Add(tockaDobar);
+            }
+
             try
             {
-                MeetingRequest.AddMeeting(meetingRequest, 0);
+                MeetingRequest.AddMeeting(meetingRequestDobar, 0);
                 return Created(new { message = "Meeting has been added." });
             }
             catch (Exception ex) {
