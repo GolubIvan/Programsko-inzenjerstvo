31ad41ea2aaf5e6c04c9dab0b1e8ce9470f8dc1d
ivan <ig54902>
2025-01-22 18:17:09 +0100

    added swagger

--- izvorniKod/backend/Backend/Program.cs
+++ izvorniKod/backend/Backend/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAnyOrigin",
@@ -10,7 +12,13 @@ builder.Services.AddCors(options =>
                             .AllowAnyHeader()); // Allow any header
 });
 
+
+// Add Swagger services
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Enable CORS policy
 app.UseCors("AllowAnyOrigin");
 
 // Configure the HTTP request pipeline.
@@ -19,6 +27,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Enable Swagger in the development environment
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.RoutePrefix = "swagger"; // Swagger UI will be at /swagger
+    });
 }
 
 app.UseHttpsRedirection();
